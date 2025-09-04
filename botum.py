62f7edda0af98c9e41e08703854a73c9dcde59eb
acemogluefe-lang <acemogluefe@gmail.com>
2025-09-04 21:22:19 +0300

    botum.py

---
+++ botum.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command(name='mainbot')
+async def _bot(ctx):
+    """Mainbot?"""
+    await ctx.send('We have Paul walker he has so much code you can enjoy.')
+
+
+
+bot.run("")
